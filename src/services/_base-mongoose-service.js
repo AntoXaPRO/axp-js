7035c5c63b7efa7f82170c2038e901d2eb303c34
AntoXa PRO <info@antoxa.pro>
2020-11-05 13:27:29 +0300

    Base mongoose service

--- src/services/_base-mongoose-service.js
+++ src/services/_base-mongoose-service.js
@@ -1,37 +1,109 @@
 import { DataResultCollectionEntity, DataResultEntity } from '../entities'
 
 export default class _BaseMongooseService {
-    constructor(model, { select, populate, sort }, formModel){
+    constructor(model, { fields, populate, sort }, formModel){
         this._model = model,
         this._options = {
-            select: select || [],
+            fields: fields || [],
             populate: populate || [],
             sort: sort || { dateCreate: 'desc' }
         }
         this._formModel = formModel
     }
 
-    _returnData(query, callback){
+    /**
+     * Приватный метод для валидация данных по модели.
+     * @param { Object } obj - Объект сущности.
+     */
+    _validFormModel(obj){
+        let result = { obj }
+        if(this._formModel){
+            const model = new this._formModel(obj)
+            if(model.isValid()){
+                result.obj = model.convertPreSave()
+            }else{
+                result.errors = model.errors
+            }
+        }
+        return result
+    }
+
+    /**
+     * Приватный метод для обработки результата создания/редактирования.
+     * @param { Promise } promise - Промис выполнения сохранения сущности в БД.
+     */
+    _returnResultOrCllback(promise, callback, successStatus = 200){
+        // Возвращаем данные.
         if(callback){
             const dataResult = new DataResultEntity()
-            query.then(doc => {
-                if(doc){
-                    dataResult.data = doc
-                }else{
-                    dataResult.status = 404
-                    dataResult.message = 'Not Found'
-                    dataResult.errors.push({ code: 'not_found', text: 'The resource is not found' })
-                }
+            promise.then(doc => {
+                dataResult.status = successStatus
+                dataResult.message = 'The data was successfully stored'
+                dataResult.data = doc
             }).catch(ex => {
                 dataResult.status = 500
                 dataResult.message = 'Server Error'
-                dataResult.errors.push({ code: 'server', text: ex.message })
+                dataResult.errors = ex
             }).finally(() => {
                 callback(dataResult)
             })
         }else{
-            return query
+            return promise
         }
+    }
+
+    /**
+     * Создание сущности.
+     * @param { Object } obj - Объект сущности.
+     * @param { Function } callback - Функция обратного вызова (аргумент функции DataResultEntity)
+     */
+    create(obj, callback){
+        // Валидация данных.
+        const validResult = this._validFormModel(obj)
+
+        // Промис обновления.
+        const promise = new Promise((resolve, reject) => {
+            if(validResult.errors){
+                reject(validResult.errors)
+            }else{
+                this._model.create(validResult.obj)
+                .then(doc => resolve(doc))
+                .catch(ex => reject([{ code: 'create', text: ex.message }]))
+            }
+        })
+
+        // Возвращаем данные.
+        return this._returnResultOrCllback(promise, callback, 201)
+    }
+
+    /**
+     * Редактирование сущности.
+     * @param { Object } obj - Объект сущности.
+     * @param { Function } callback - Функция обратного вызова (аргумент функции DataResultEntity)
+     */
+    update(obj, callback){
+
+        // Обновляем даты.
+        const newDate = Date.now()
+        obj.dateCreate = obj.dateCreate ? obj.dateCreate : newDate
+        obj.dateUpdate = newDate
+
+        // Валидация данных.
+        const validResult = this._validFormModel(obj)
+
+        // Промис обновления.
+        const promise = new Promise((resolve, reject) => {
+            if(validResult.errors){
+                reject(validResult.errors)
+            }else{
+                this._model.findByIdAndUpdate(validResult.obj._id, validResult.obj, { new: true, runValidators: true })
+                .then(doc => resolve(doc))
+                .catch(ex => reject([{ code: 'update', text: ex.message }]))
+            }
+        })
+
+        // Возвращаем данные.
+        return this._returnResultOrCllback(promise, callback)
     }
 
     /**
@@ -40,73 +112,37 @@ export default class _BaseMongooseService {
      * @param { Function } callback - Функция обратного вызова (аргумент функции DataResultEntity)
      */
     createOrUpdate (obj, callback){
-
-        let validErrors = []
-        const newDate = new Date()
-        obj.dateCreate = obj.dateCreate ? obj.dateCreate : newDate
-        obj.dateUpdate = newDate
-
-        // Валидация данных.
-        if(this._formModel){
-            try{
-                const model = new this._formModel(obj)
-                if(model.isValid()){
-                    obj = model.convertPreSave()
-                }else{
-                    validErrors = model.errors
-                }
-            }catch(ex){
-                validErrors = [{ code: 'valid', text: ex.message }]
-            }
-        }
-
-        // Промис.
-        const promise = new Promise((resolve, reject) => {
-            if(validErrors && validErrors.length > 0){
-                reject(validErrors)
-            }else{
-                this._model.findByIdAndUpdate(obj._id, obj, { new: true, upsert: true, runValidators: true })
-                .then(doc => resolve(doc))
-                .catch(ex => reject([{ code: 'server_db', text: ex.message }]))
-            }
-        })
-
-        // Возвращаем данные.
-        if(callback){
-            const dataResult = new DataResultEntity()
-            if(validErrors && validErrors.length > 0){
-                dataResult.status = 401
-                dataResult.message = 'Valid error'
-                dataResult.errors = validErrors
-                callback(dataResult)
-            }else{
-                promise.then(doc => {
-                    dataResult.status = obj._id ? 201 : 200
-                    dataResult.message = 'The data was successfully stored.'
-                    dataResult.data = doc
-                }).catch(ex => {
-                    dataResult.status = 500
-                    dataResult.message = 'Server Error'
-                    dataResult.errors = ex
-                }).finally(() => {
-                    callback(dataResult)
-                })
-            }
+        if(obj._id){
+            return this.update(obj, callback)
         }else{
-            return promise
+            return this.create(obj, callback)
         }
+    }
+
+    /**
+     * Удаление сущности из БД.
+     * @param { String } id - ИД сущности в БД.
+     * @param { Function } callback - Функция обратного вызова (аргумент функции DataResultEntity)
+     */
+    remove(id, callback){
+        const promise = new Promise((resolve, reject) => {
+            this._model.findByIdAndRemove(id)
+            .then(doc => resolve(doc))
+            .catch(ex => reject([{ code: 'remove', text: ex.message }]))
+        })
+        return this._returnResultOrCllback(promise, callback)
     }
 
     /**
      * Возвращает коллекцию.
      * @param { Object } filter - Объект фильтра для выборки из БД.
-     * @param { Object } options - Допю настройка для выборки (select, populate, sort)
+     * @param { Object } options - Доп. настройка для выборки (select, populate, sort)
      * @param { Function } callback - Функция обратного вызова (аргумент функции DataResultCollectionEntity)
      */
     find (filter = {}, options = {}, callback) {
 
         const query = this._model.find(filter)
-        .select(options.select || this._options.select)
+        .select(options.filter || this._options.fields)
         .populate(options.populate || this._options.populate)
         .sort(options.sort || this._options.sort)
 
@@ -129,15 +165,40 @@ export default class _BaseMongooseService {
 
     findById (id, options = {}, callback) {
         const query = this._model.findById(id)
-        .select(options.select || this._options.select)
+        .select(options.filter || this._options.fields)
         .populate(options.populate || this._options.populate)
         return this._returnData(query, callback)
     }
 
     findOne (filter, options = {}, callback) {
         const query = this._model.findOne(filter)
-        .select(options.select || this._options.select)
+        .select(options.filter || this._options.fields)
         .populate(options.populate || this._options.populate)
         return this._returnData(query, callback)
+    }
+
+    /* 
+    Нужно удалить */
+    _returnData(query, callback){
+        if(callback){
+            const dataResult = new DataResultEntity()
+            query.then(doc => {
+                if(doc){
+                    dataResult.data = doc
+                }else{
+                    dataResult.status = 404
+                    dataResult.message = 'Not Found'
+                    dataResult.errors.push({ code: 'not_found', text: 'The resource is not found' })
+                }
+            }).catch(ex => {
+                dataResult.status = 500
+                dataResult.message = 'Server Error'
+                dataResult.errors.push({ code: 'server', text: ex.message })
+            }).finally(() => {
+                callback(dataResult)
+            })
+        }else{
+            return query
+        }
     }
 }
